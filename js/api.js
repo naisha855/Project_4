07fc9852c8d74e69bb81d6e60609210025246583
naisha855 <156628341+naisha855@users.noreply.github.com>
2024-03-25 17:38:09 +0900

    2024-03-25_01

--- js/api.js
+++ js/api.js
@@ -1,49 +1,50 @@
-$(function(){
+$(function () {
 
-let search = ["바람", "영화", "달콤한", "초록", "보리", "차트",
- "여행", "노랑", "새", "음식"];
+    let search = ["바람", "영화", "달콤한", "초록", "보리", "차트",
+        "여행", "노랑", "새", "음식"];
 
-for (let i = 0; i < search.length; i++) {
+    for (let i = 0; i < search.length; i++) {
 
-    $.ajax({
-        method: "GET",
-        url: "https://dapi.kakao.com/v3/search/book?target=title",
-        data: { query: search[i] },
-        async: false,
-        headers: { Authorization: "KakaoAK 7d38ca298160bac7d36fd3c326771778" }
-    })
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: search[i] },
+            async: false,
+            headers: { Authorization: "KakaoAK 7d38ca298160bac7d36fd3c326771778" }
+        })
 
-        .done(function (msg) {
-            $(".section3_main_populater_container_list").eq(i).find('figure>a').append("<img src='" + msg.documents[i].thumbnail + "'/>");
-            $(".section3_main_populater_container_list").eq(i).find('h3').append("<a href=''>" + msg.documents[i].title + "</a>");
-            $(".section3_main_populater_container_list").eq(i).find('.section3_main_populater_container_list_inner_detail_price').append("<span>" + msg.documents[i].price + "</span>");
+            .done(function (msg) {
+                $(".section3_main_populater_container_list").eq(i).find('figure>a').append("<img src='" + msg.documents[i].thumbnail + "'/>");
+                $(".section3_main_populater_container_list").eq(i).find('h3').append("<a href=''>" + msg.documents[i].title + "</a>");
+                $(".section3_main_populater_container_list").eq(i).find('.section3_main_populater_container_list_inner_detail_price').append("<span>" + msg.documents[i].price + "</span>");
 
-        });
+            });
+    }
+
+    let search2 = ["과학", "죄와 벌", "삶", "동화", "기원"];
+
+    for (let j = 0; j < search2.length; j++) {
+
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: search2[j] },
+            async: false,
+            headers: { Authorization: "KakaoAK 7d38ca298160bac7d36fd3c326771778" }
+        })
+
+
+
+            .done(function (msg) {
+                console.log(msg)
+                $(".section3_main_subpopulate .section3_main_populater_container_list").eq(j).find('figure>a').append("<img src='" + msg.documents[j].thumbnail + "'/>");
+                $(".section3_main_subpopulate .section3_main_populater_container_list").eq(j).find('h3').append("<a href=''>" + msg.documents[j].title + "</a>");
+                $(".section3_main_subpopulate .section3_main_populater_container_list").eq(j).find('.section3_main_populater_container_list_inner_detail_price').append("<span>" + msg.documents[j].price + "</span>");
+
+                let str = msg.documents[j].title;
+                let str2 = str.substring(0, 2)
+
+            });
+    }
 }
-
-let search2 = ["과학", "죄와 벌", "삶", "동화", "기원"];
-
-for (let j = 0; j < search2.length;j++) {
-
-    $.ajax({
-        method: "GET",
-        url: "https://dapi.kakao.com/v3/search/book?target=title",
-        data: { query: search2[j] },
-        async: false, 
-        headers: { Authorization: "KakaoAK 7d38ca298160bac7d36fd3c326771778" }
-    })
-
-
-
-        .done(function (msg) {
-            console.log(msg)
-            $(".section3_main_subpopulate .section3_main_populater_container_list").eq(j).find('figure>a').append("<img src='" + msg.documents[j].thumbnail + "'/>");
-            $(".section3_main_subpopulate .section3_main_populater_container_list").eq(j).find('h3').append("<a href=''>" + msg.documents[j].title + "</a>");
-            $(".section3_main_subpopulate .section3_main_populater_container_list").eq(j).find('.section3_main_populater_container_list_inner_detail_price').append("<span>" + msg.documents[j].price + "</span>");
-
-            let str = msg.documents[j].title;
-            let str2 = str.substring(0, 2)
-
-        });
-}
-})
+)
---
+++ js/bookinfo.js
@@ -0,0 +1,54 @@
+$(function () {
+
+    let search = ["solateintheday", "walkthebluefields", "맡겨진 소녀",];
+
+    for (let i = 0; i < search.length; i++) {
+
+        $.ajax({
+            method: "GET",
+            url: "https://dapi.kakao.com/v3/search/book?target=title",
+            data: { query: search[i] },
+            async: false,
+            headers: { Authorization: "KakaoAK 7d38ca298160bac7d36fd3c326771778" }
+        })
+
+            .done(function (msg) {
+                if(i==1){
+                    $(".Section_3_container_part3_populated_container_list_populeted").eq(i).find('figure>a').append("<img src='" + msg.documents[1].thumbnail + "'/>");
+                    $(".Section_3_container_part3_populated_container_list_populeted").eq(i).find('h3').append("<a href=''>" + msg.documents[1].title + "</a>");
+                    $(".Section_3_container_part3_populated_container_list_populeted").eq(i).find('.Section_3_container_part3_populated_container_list_detail_price').append(msg.documents[1].price);
+                }else{
+                    $(".Section_3_container_part3_populated_container_list_populeted").eq(i).find('figure>a').append("<img src='" + msg.documents[0].thumbnail + "'/>");
+                $(".Section_3_container_part3_populated_container_list_populeted").eq(i).find('h3').append("<a href=''>" + msg.documents[0].title + "</a>");
+                $(".Section_3_container_part3_populated_container_list_populeted").eq(i).find('.Section_3_container_part3_populated_container_list_detail_price').append(msg.documents[0].price);
+                }
+                
+
+            });
+
+
+
+        //       $(".contextbox").append("<h6>" + msg.documents[0].authors[0] + "</h6>");
+
+        //       let str = msg.documents[0].contents;
+        //       let str2 = str.substring(0, 600);
+        //       $(".contextbox").append("<p>" + str2 + "</p>");
+        //     });
+
+
+
+    }
+    $(function () {
+        $(".Section_2_main_populate_text").append("자세히보기");
+        $(".Section_3_container_part1_populated_text").append("자세히보기");
+    })
+
+    $(function () {
+        $.get("./txt/bookinfo_small_things_like_these.txt", function (data) {
+            $(".Section_2_main_populate_text").html(data);
+        })
+        $.get("./txt/Claire_keegan.txt", function (data) {
+            $(".Section_3_container_part1_populated_text").html(data);
+        })
+    });
+});
